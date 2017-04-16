0c96f32949569b95b81c7ffb6846290cf966a62c
Gema Parreño <gema.parreno.piqueras@gmail.com>
2017-04-16 21:37:32 +0200

    making th video

--- SpaceInvadersDeterministic2.py
+++ SpaceInvadersDeterministic2.py
@@ -16,3 +16,34 @@ for i_episode in range(120):
 #Box and Discrete are the most normal environments 
 print(env.action_space)
 print(env.observation_space)
+
+#Save the environment into a video#
+
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+# Define the codec and create VideoWriter object
+#fourcc = cv2.cv.CV_FOURCC(*'DIVX')
+#out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+out = cv2.VideoWriter('output.avi', -1, 20.0, (640,480))
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret==True:
+        frame = cv2.flip(frame,0) #TODO: see if its 0 or 180
+
+        # write the flipped frame
+        out.write(frame)
+
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+# Release everything if job is finished
+cap.release()
+out.release()
+cv2.destroyAllWindows()
